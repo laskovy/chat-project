7bf9d957fcca3f88f15858e9ab2335437e224a47
laskovy <laskovyjsvatoslav@gmail.com>
2026-06-02 17:20:05 +0300

    left pannel and new models

--- chat_app/models.py
+++ chat_app/models.py
@@ -1,12 +1,14 @@
 from project.db import *
-from main_app.models import User
+from main_app.models import User, group_users
 
 class Group(DATA_BASE.Model):
     id = DATA_BASE.Column(DATA_BASE.Integer, primary_key=True)
     name = DATA_BASE.Column(DATA_BASE.String, nullable=False)
     creator_id = DATA_BASE.Column(DATA_BASE.Integer, DATA_BASE.ForeignKey("user.id"))
 
-    creator = DATA_BASE.relationship("User", back_populates="groups")
+    creator = DATA_BASE.relationship("User", back_populates="groups_created")
+    members = DATA_BASE.relationship("User", secondary=group_users, back_populates="groups")
+
     messages = DATA_BASE.relationship("Message", back_populates="group")
 
 
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from project.db import*
 from flask_login import UserMixin
 
+
+
+group_users = DATA_BASE.Table(
+    "group_users",
+    DATA_BASE.Column("user_id", DATA_BASE.Integer, DATA_BASE.ForeignKey("user.id"), primary_key=True),
+    DATA_BASE.Column("group_id", DATA_BASE.Integer, DATA_BASE.ForeignKey("group.id"), primary_key=True)
+)
 class User(DATA_BASE.Model, UserMixin):
     id = DATA_BASE.Column(DATA_BASE.Integer, primary_key=True)
     email = DATA_BASE.Column(DATA_BASE.String, unique=True, nullable=False)
@@ -8,5 +15,8 @@ class User(DATA_BASE.Model, UserMixin):
     is_active = DATA_BASE.Column(DATA_BASE.Boolean, default=False)
     verify_code = DATA_BASE.Column(DATA_BASE.String, nullable=True)
 
-    groups = DATA_BASE.relationship("Group", back_populates="creator")
+
+    groups_created = DATA_BASE.relationship("Group", back_populates="creator")
+    groups = DATA_BASE.relationship("Group", secondary=group_users, back_populates="members")
     messages = DATA_BASE.relationship("Message", back_populates="user")
+    
